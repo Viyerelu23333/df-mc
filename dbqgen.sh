48346c31846550207560976f4d6de1ea2da0cea2
Viyerelu23333 <11363469+Viyerelu23333@users.noreply.github.com>
2022-02-18 21:24:29 -0500

    feat: add qgen default mode

--- dbqgen.sh
+++ dbqgen.sh
@@ -4,6 +4,7 @@ SF=2
 OUT_DIR='./dbqgen'
 EXEC_DIR='./dbgen'
 CWD=$(pwd)
+QGEND=''
 
 err() {
     echo >&2 "$1: $2"
@@ -27,9 +28,13 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
+        -qd|--qgen-default)
+            QGEND='-d'
+            shift
+            ;;
         *)
             echo "Unknown option: $1"
-            echo "Possible options: -s <ScaleFactor> -o <OutputDir> -e <ExecutableDir>"
+            echo "Possible options: -s <ScaleFactor> -o <OutputDir> -e <ExecutableDir> [-qd]"
             exit 1
     esac
 done
@@ -51,7 +56,7 @@ cd "queries" || err "Failed to locate queries directory"
 mkdir --parents "$CWD/$OUT_DIR/queries" || err "Failed to make folder"
 
 for sql in {1..22}; do
-    ../qgen -b ../dists.dss -s $SF $sql > "$CWD/$OUT_DIR/queries/$sql.sql" || err "Failed to qgen $sql.sql"
+    ../qgen -b ../dists.dss -s $SF $QGEND $sql > "$CWD/$OUT_DIR/queries/$sql.sql" || err "Failed to qgen $sql.sql"
 done
 
 cd "$CWD"
